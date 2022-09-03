f00eb7ebca27566702876fd1ca7ee769afd8a4ce
nikola.balic <nikola.balic@gmail.com>
2022-09-03 05:35:29 -0400

    rss fix 2

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -4,6 +4,7 @@ const postImportResult = import.meta.globEager("./posts/*.md");
 const posts = Object.values(postImportResult);
 
 export const get = () => rss({
+    stylesheet: '/rss/styles.xsl',
     title: 'Pulent Blog',
     description: 'Nurturing growth while fighting perfectionism',
     site: import.meta.env.SITE,
@@ -11,5 +12,6 @@ export const get = () => rss({
       link: post.url,
       title: post.frontmatter.title,
       pubDate: post.frontmatter.date,
+      description: post.frontmatter.description
     }))
   });
