3eb4e549bbe9e68a8aed2621e3cf3333e5b6d6c8
nikola.balic <nikola.balic@gmail.com>
2022-09-03 05:23:48 -0400

    rss fix

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,10 +3,13 @@ import rss from "@astrojs/rss";
 const postImportResult = import.meta.globEager("./posts/*.md");
 const posts = Object.values(postImportResult);
 
-export const get = () =>
-  rss({
-    title: "Astro Theme Creek",
-    description: "A Theme for Astro",
+export const get = () => rss({
+    title: 'Pulent Blog',
+    description: 'Nurturing growth while fighting perfectionism',
     site: import.meta.env.SITE,
-    items: import.meta.glob("./posts/**/*.md"),
+    items: posts.map((post) => ({
+      link: post.url,
+      title: post.frontmatter.title,
+      pubDate: post.frontmatter.date,
+    }))
   });
