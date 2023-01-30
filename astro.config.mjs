a322f332e43237a81772eb5d86747908e3a6d8c8
nikola.balic <nikola.balic@gmail.com>
2023-01-30 14:55:10 -0500

    collections

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,7 +12,37 @@ export default defineConfig({
                 branch: 'main',
               },
               collections: [
-                // Content collections
+                {
+                    name: 'posts',
+                    label: 'Posts',
+                    label_singular: 'Post',
+                    folder: 'src/pages/posts',
+                    create: true,
+                    delete: true,
+                    fields: [
+                      { name: 'title', widget: 'string', label: 'Post Title' },
+                      {
+                        name: 'publishDate',
+                        widget: 'datetime',
+                        format: 'DD MMM YYYY',
+                        date_format: 'DD MMM YYYY',
+                        time_format: false,
+                        label: 'Publish Date',
+                      },
+                      { name: 'author', widget: 'string', label: 'Author Name', required: false },
+                      { name: 'authorURL', widget: 'string', label: 'Author URL', required: false },
+                      { name: 'description', widget: 'string', label: 'Description', required: false },
+                      { name: 'body', widget: 'markdown', label: 'Post Body' },
+                      {
+                        name: 'layout',
+                        widget: 'select',
+                        default: '../../layouts/BlogPosLayout.astro',
+                        options: [
+                          { label: 'Post', value: '../../layouts/BlogPosLayout.astro' },
+                        ],
+                      },
+                    ],
+                },
               ],
             },
           }),
