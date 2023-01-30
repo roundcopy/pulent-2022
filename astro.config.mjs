c43be8875b3a1f1b1a17273e4233ea7917ee397e
nikola.balic <nikola.balic@gmail.com>
2023-01-30 14:26:04 -0500

    netlify cms

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,21 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
+import NetlifyCMS from 'astro-netlify-cms';
 
 export default defineConfig({
-  integrations: [sitemap()],
+  integrations: [
+        sitemap(),
+        NetlifyCMS({
+            config: {
+              backend: {
+                name: 'git-gateway',
+                branch: 'main',
+              },
+              collections: [
+                // Content collections
+              ],
+            },
+          }),
+    ],
   site: "https://pulent.com",
 });
